83d43fe7fc7b4d574b4968a141d005121405069f
redcliphaloe <redcliphaloe@gmail.com>
2022-05-22 21:49:30 +0300

    feat: change output to stdout in 01-read-file (Sun, May 22, 2022 09:49:30 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,4 +1,5 @@
+const process = require('process');
 const path = require('path');
 const fs = require('fs');
 const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
-rs.on('data', chunk => console.log(chunk));
+rs.on('data', chunk => process.stdout.write(chunk));
