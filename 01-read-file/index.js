7bcdf123bd0ee096ef4da2bee24c66ce260a4fcc
redcliphaloe <redcliphaloe@gmail.com>
2022-05-22 19:54:15 +0300

    feat: add path to 01-read-file (Sun, May 22, 2022 07:54:15 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,4 @@
-const fs = require('node:fs');
-const rs = fs.createReadStream('01-read-file\\text.txt', 'utf8');
+const path = require('path');
+const fs = require('fs');
+const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
 rs.on('data', chunk => console.log(chunk));
