1fb226d2e211730bf0a496260d2b6a02b62670e7
redcliphaloe <redcliphaloe@gmail.com>
2022-05-23 18:00:54 +0300

    refactor: correct arrow function style in 01-read-file (Mon, May 23, 2022 06:00:54 PM)

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,4 +2,4 @@ const process = require('process');
 const path = require('path');
 const fs = require('fs');
 const rs = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf8');
-rs.on('data', chunk => process.stdout.write(chunk));
+rs.on('data', (chunk) => {process.stdout.write(chunk)});
