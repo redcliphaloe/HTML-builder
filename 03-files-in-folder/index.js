fac61446acca43929fb76bf70c750bc280ddf633
redcliphaloe <redcliphaloe@gmail.com>
2022-05-23 20:20:40 +0300

    feat: complete 03-files-in-folder (Mon, May 23, 2022 08:20:40 PM)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const process = require('process');
+const path = require('path');
+const fs = require('fs');
+const filesInFolder = async (folderName) => {    
+    try {        
+        const files = await fs.promises.readdir(path.join(__dirname, folderName), {withFileTypes: true});
+        for (const file of files) {
+            let stats = await fs.promises.stat(path.join(__dirname, folderName, file.name));
+            if (stats.isFile()) {
+                const name = file.name.split('.')[0];
+                const ext = file.name.split('.')[1];
+                const size = stats.size;
+                process.stdout.write(`${name} - ${ext} - ${size} bytes\n`);
+            }
+        }          
+      } catch (err) {
+          process.stdout.write(err.message);
+      }    
+};
+filesInFolder('secret-folder');
